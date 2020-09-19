a4024a13d636978817a5af5602c164d8b2908fe5
mtsub <mtsub@MT-ZENBOOK.mshome.net>
2020-09-19 15:35:33 +0900

    Version 0.3

--- learn-java-orderapp/src/main/java/com/example/orderapp/logic/ItemLogicException.java
+++
@@ -1,9 +0,0 @@
-package com.example.orderapp.logic;
-
-public class ItemLogicException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public ItemLogicException(String message) {
-		super(message);
-	}
-}
